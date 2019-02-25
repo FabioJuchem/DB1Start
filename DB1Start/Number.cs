375973000729686b5e66570ddedd09033783a3fe
FabioJuchem <fabiojuchem@gmail.com>
2019-02-25 14:14:52 -0300

    Adicionado codigo fonte teste DB1Start

---
+++ DB1Start/Number.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace DB1Start
+{
+    class Number
+    {
+        public double Value { get;private set ; }
+        public string Even { get; set; }
+        public string Odds { get; set; }
+        public double DoubleValue { get; set; }
+        public string Prime { get; set; }
+
+        public Number()
+        {
+        }
+
+        public Number(double value)
+        {
+            Value = value;   
+        }
+
+
+        public void EvenOdds()
+        {
+            if(Value == 1)
+            {
+                Even = "Numero Neutro";
+                
+            }
+            else if(Value % 2 == 0)
+            {
+                Even = "é Par";
+                Odds = "Não é Impar";
+            }
+            else
+            {
+                Even = "Não é Par";
+                Odds = "É Impar";
+            }
+        }
+
+        public void Double()
+        {
+            DoubleValue = Value * 2;
+        }
+
+        public void PrimeNumber()
+        {
+            for (double i = 2; i < Value; i++)
+            {
+                double resto = Value % i;
+                if (resto == 0)
+                {
+                    Prime = "Não é um número primo";
+                    i = Value + 1;
+                }
+                else
+                {
+                    Prime ="É um número primo";
+                }
+            }
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("O numero : ");
+            sb.AppendLine(Value.ToString("F2",CultureInfo.InvariantCulture));
+            sb.AppendLine(Even);
+            sb.AppendLine(Odds);
+            if(Value > 10)
+            {
+                sb.AppendLine("O Numero é maior que 10!!");
+            }
+            else if(Value == 10)
+            {
+                sb.AppendLine("O numero é igual a 10");
+            }
+            else
+            {
+                sb.AppendLine("O Numero é menor que 10!!");
+            }
+            sb.Append("O Dobro de "+Value.ToString("F2",CultureInfo.InvariantCulture)+" é ");
+            sb.AppendLine(DoubleValue.ToString("F2",CultureInfo.InvariantCulture));
+            sb.AppendLine(Prime);
+            return sb.ToString();
+           
+        }
+    }
+}
---
+++ DB1Start/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace DB1Start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Quantos numeros irá digitar?");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Informe um numero: ");
+                double number = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Number informedNumber = new Number(number);
+                informedNumber.Double();
+                informedNumber.EvenOdds();
+                informedNumber.PrimeNumber();
+                Console.WriteLine(informedNumber);
+            }
+
+        }
+    }
+}
